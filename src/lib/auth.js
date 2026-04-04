a3fc1a686682fc787d59824c941bcac79b5dcb2f
Petros Asmamaw <petrosasmamaw2@gmail.com>
2026-04-04 15:45:14 +0300

    add our url in trusted origin

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -12,6 +12,10 @@ export const auth = betterAuth({
     database: prismaAdapter(prisma, { provider: "postgresql" }),
     secret: process.env.BETTER_AUTH_SECRET,
     baseURL: process.env.BETTER_AUTH_URL,
+    trustedOrigins: [
+        "http://localhost:3000",
+        "https://qandil-ai.vercel.app"
+    ],
     
     emailAndPassword: {
         enabled: true,
