8c483a6365027ee5a82cf60097292881dde9f3b6
Petros Asmamaw <petrosasmamaw2@gmail.com>
2026-04-04 16:26:45 +0300

    update autclient

--- src/lib/authClient.js
+++ src/lib/authClient.js
@@ -2,4 +2,5 @@ import { createAuthClient } from "better-auth/react"
 
 export const authClient = createAuthClient({
     // use same origin by default; set NEXT_PUBLIC_AUTH_BASE_URL for custom base
+    baseURL: process.env.NEXT_PUBLIC_AUTH_BASE_URL || ""
 })
