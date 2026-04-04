1c564f27926c35a833041e04a54b3e4c81d4854a
Petros Asmamaw <petrosasmamaw2@gmail.com>
2026-04-04 15:50:31 +0300

    remove NEXT_PUBLIC_AUTH_BASE_URL from auth client

--- src/lib/authClient.js
+++ src/lib/authClient.js
@@ -2,5 +2,4 @@ import { createAuthClient } from "better-auth/react"
 
 export const authClient = createAuthClient({
     // use same origin by default; set NEXT_PUBLIC_AUTH_BASE_URL for custom base
-    baseURL: process.env.NEXT_PUBLIC_AUTH_BASE_URL || ""
 })
